9a3c9963960cda8d19a31870f0ae214ab977a7b6
RAFAEL FERNANDES <rafael.net.ca@gmail.com>
2015-05-05 09:18:05 -0400

    More tests

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ function test(){
 function sum(a, b){
 	return a+b;
 }
+
+function subtract(a, b){
+	return a-b;
+}
+
+function multiply(a,b){
+		return a*b;
+}
+
+function sumByFive(num){
+	return sum(num,5);
+}
+
+function subtractByTwiceItsValue(num){
+	return subtract(num, multiply(num,2));
+}
--- test/test.spec.js
+++ test/test.spec.js
@@ -9,4 +9,12 @@ describe('Test', function() {
   it('should sum', function() {
     expect(sum(2,3)).toBe(5);
   });
+
+  it('should sum by five', function(){
+  	expect(sumByFive(10)).toBe(15);
+  })
+
+  it('should subtract by twice its value', function(){
+  	expect(subtractByTwiceItsValue(10)).toBe(-10);
+  })
 });
